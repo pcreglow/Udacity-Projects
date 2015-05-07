09fe173c4840daa59a6c00bd2bc225d47fa9ceef
Phillip Creglow <phillipcreglow@msn.com>
2015-05-07 16:33:38 -0400

    Added ifs 0 - 10 Test: Passed

--- string-calculator.js
+++ string-calculator.js
@@ -40,3 +40,11 @@ function toEnglish(word){
 expect(toEnglish("zero")).to.equal(0);
 expect(toEnglish("one")).to.equal(1);
 expect(toEnglish("two")).to.equal(2);
+expect(toEnglish("three")).to.equal(3);
+expect(toEnglish("four")).to.equal(4);
+expect(toEnglish("five")).to.equal(5);
+expect(toEnglish("six")).to.equal(6);
+expect(toEnglish("seven")).to.equal(7);
+expect(toEnglish("eight")).to.equal(8);
+expect(toEnglish("nine")).to.equal(9);
+expect(toEnglish("ten")).to.equal(10);
