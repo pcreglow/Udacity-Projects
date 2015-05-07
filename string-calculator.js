0700550d526731a0aaf88fb94a112f4292972162
Phillip Creglow <phillipcreglow@msn.com>
2015-05-07 16:25:03 -0400

    Added if statements, 0 - 10

--- string-calculator.js
+++ string-calculator.js
@@ -11,7 +11,30 @@ function toEnglish(word){
   if ( word === "two" ){
       return 2;
   }
-
+  if ( word === "three" ){
+      return 3;
+  }
+  if ( word === "four" ){
+      return 4;
+  }
+  if ( word === "five"){
+      return 5;
+  }
+  if ( word === "six"){
+      return 6;
+  }
+  if ( word === "seven"){
+    return 7;
+  }
+  if ( word === "eight"){
+    return 8;
+  }
+  if ( word === "nine" ){
+    return 9;
+  }
+  if ( word === "ten" ){
+    return 10;
+  }
 }
 
 expect(toEnglish("zero")).to.equal(0);
