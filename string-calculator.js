56a90c17293f7a7b205e18ac6d7feb142057a350
Phillip Creglow <phillipcreglow@msn.com>
2015-05-08 08:45:52 -0400

    Test Fail - string-calculator add(A,B) function# Please enter the commit message for your changes. Lines starting

--- string-calculator.js
+++ string-calculator.js
@@ -48,3 +48,12 @@ expect(toEnglish("seven")).to.equal(7);
 expect(toEnglish("eight")).to.equal(8);
 expect(toEnglish("nine")).to.equal(9);
 expect(toEnglish("ten")).to.equal(10);
+
+function add(A, B){
+
+}
+
+expect(plus).to.exist;
+expect(plus("zero", "zero")).to.equal(0);
+expect(plus("zero", "one")).to.equal(1);
+expect(plus("zero", "two")).to.equal(2);
