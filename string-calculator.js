af5c0ac48a7deee44b2fe4fe327e87eef2b7be79
Phillip Creglow <phillipcreglow@msn.com>
2015-05-08 11:38:26 -0400

    Test #2: Nick/Phil function(plus) FAILED
    @pope410211 @pcreglow

--- string-calculator.js
+++ string-calculator.js
@@ -70,4 +70,19 @@ expect(plus("zero", "nine")).to.equal(9);
 // console.log(plus("zero", "two"));
 
 expect(plus("one", "one")).to.equal(2);
+expect(plus("one", "two")).to.equal(3);
+expect(plus("one", "three")).to.equal(4);
+expect(plus("one", "four")).to.equal(5);
+expect(plus("one", "five")).to.equal(6);
+expect(plus("one", "six")).to.equal(7);
+expect(plus("one", "seven")).to.equal(8);
+expect(plus("one", "eight")).to.equal(9);
+expect(plus("one", "nine")).to.equal(10);
+
+expect(plus("two", "zero")).to.equal(2);
+expect(plus("two", "one")).to.equal(3);
+expect(plus("two", "two")).to.equal(4);
+expect(plus("two", "three")).to.equal(5);
+expect(plus("two", "four")).to.equal(6);
+expect(plus("two", "five")).
 console.log(plus("one", "one"));
